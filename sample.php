df7898118c8a0f9e30ced2dba1f680efa6ebead0
shou.fukuhara <sho.fukuhara0213@gmail.com>
2020-12-10 14:12:38 +0000

    add omikuji function

--- sample.php
+++ sample.php
@@ -1,5 +1,13 @@
 <?php
     $result = '無し';
+    if (array_key_exists('act',$_PORT)) {
+        $result = omikuji();
+    }
+    
+    function omikuji() {
+        $fortune = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"];
+        return $fortune[rendom_int(0, count($fortune) - 1)];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
